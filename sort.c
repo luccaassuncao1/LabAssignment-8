272af13d336bf60db0f76d25afb2c0b8699ddcbf
luccaassuncao1 <125625347+luccaassuncao1@users.noreply.github.com>
2023-04-09 14:57:27 -0300

    Update sort.c

--- sort.c
+++ sort.c
@@ -4,26 +4,21 @@
 
 int extraMemoryAllocated;
 
-// Heapify subtree rooted at index i
 void heap(int arr[], int n, int i)
 {
     int largest = i; // Initialize largest as root
     int l = 2*i + 1; // left = 2*i + 1
     int r = 2*i + 2; // right = 2*i + 2
  
-    // If left child is larger than root
     if (l < n && arr[l] > arr[largest])
         largest = l;
  
-    // If right child is larger than largest so far
     if (r < n && arr[r] > arr[largest])
         largest = r;
  
-    // If largest is not root
     if (largest != i) {
         swap(arr[i], arr[largest]);
- 
-        // Recursively heapify the affected sub-tree
+
         heap(arr, n, largest);
     }
 }
